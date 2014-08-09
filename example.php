053f22174f1ea2e2cdb6271d0be72e85bb2ba589
Benn Meyers <mb@norse-corp.com>
2014-08-09 14:23:20 -0700

    initial structure

---
+++ example.php
@@ -0,0 +1,17 @@
+<?php include("assets/includes/global-header.php"); ?>
+<?php include("assets/includes/main-header.php"); ?>
+  <div id="form" class="container">
+      <div id="content">
+        <div id="main_content">
+          <div>
+            <h2>Example</h2>
+            <form>
+              <input type="text" name="field1" id="field1" value="" placeholder="Type a word to activate">
+            </form>
+          </div>
+        </div><!-- #main_content -->
+        <?php include("assets/includes/sub-nav.php"); ?>
+      </div><!-- #content -->
+    <?php include("assets/includes/main-footer.php"); ?>
+  </div><!-- #form -->
+<?php include("assets/includes/global-footer.php"); ?>
